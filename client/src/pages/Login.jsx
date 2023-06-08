dfe43a424089e5c11bfee8de27a777a0a63c72aa
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-06-08 14:59:28 +0530

    login responsive

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -25,53 +25,55 @@ const Login = () => {
   };
 
   return (
-    <form
-      className="p-4 p-md-5 border rounded-3 bg-body-tertiary w-50 mx-auto shadow-lg"
-      onSubmit={handleSubmit(onSubmit)}
-      noValidate
-    >
-      <Toaster />
-      <h2 className="text-center mb-3">Login</h2>
-      <div className="form-floating mb-3">
-        <input
-          type="email"
-          className="form-control"
-          id="floatingEmail"
-          placeholder="Email Address"
-          {...register("email", {
-            required: { value: true, message: "Email is Required" },
-            pattern: {
-              // eslint-disable-next-line
-              value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-              message: "Invalid Email Name Format",
-            },
-          })}
-        />
-        <label htmlFor="floatingEmail">Email Address</label>
-        <small className="text-danger">{errors.email?.message}</small>
-      </div>
-      <div className="form-floating mb-3">
-        <input
-          type="password"
-          className="form-control"
-          id="floatingPassword"
-          placeholder="Password"
-          {...register("password", {
-            required: { value: true, message: "Password is Required" },
-            pattern: {
-              value:
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/,
-              message: "Invalid Password Format",
-            },
-          })}
-        />
-        <label htmlFor="floatingPassword">Password</label>
-        <small className="text-danger">{errors.password?.message}</small>
-      </div>
-      <button className="w-100 btn btn-lg btn-primary" type="submit">
-        Login
-      </button>
-    </form>
+    <div style={{ maxWidth: "700px" }}>
+      <form
+        className="p-4 p-md-5 border rounded-3 bg-body-tertiary mx-auto shadow-lg"
+        onSubmit={handleSubmit(onSubmit)}
+        noValidate
+      >
+        <Toaster />
+        <h2 className="text-center mb-3">Login</h2>
+        <div className="form-floating mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="floatingEmail"
+            placeholder="Email Address"
+            {...register("email", {
+              required: { value: true, message: "Email is Required" },
+              pattern: {
+                // eslint-disable-next-line
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                message: "Invalid Email Name Format",
+              },
+            })}
+          />
+          <label htmlFor="floatingEmail">Email Address</label>
+          <small className="text-danger">{errors.email?.message}</small>
+        </div>
+        <div className="form-floating mb-3">
+          <input
+            type="password"
+            className="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+            {...register("password", {
+              required: { value: true, message: "Password is Required" },
+              pattern: {
+                value:
+                  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$/,
+                message: "Invalid Password Format",
+              },
+            })}
+          />
+          <label htmlFor="floatingPassword">Password</label>
+          <small className="text-danger">{errors.password?.message}</small>
+        </div>
+        <button className="w-100 btn btn-lg btn-primary" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
   );
 };
 
